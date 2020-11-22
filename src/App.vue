a4d73b3deb1df7009f8f83fa7bb6555f3f3497d0
splanes <sergi.planestor@gmail.com>
2020-11-22 01:56:31 +0100

    Last update

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,54 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar
+        app
+        color="primary"
+        dark
+    >
+      <div class="d-flex align-center">
+        <v-avatar>
+          <v-icon
+              large
+              class="orange darken-1"
+              dark
+          >
+            mdi-key
+          </v-icon>
+        </v-avatar>
+
+        <v-btn
+            href="/"
+            target="_blank"
+            text
+        >
+          <span>Secure Password</span>
+        </v-btn>
+      </div>
+
+      <v-spacer></v-spacer>
+    </v-app-bar>
+
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data: () => ({
+    //
+  }),
+  actions: {
+    onClose() {
+      window.onbeforeunload = function() {
+         alert("Are you sure you want to close the window?");
+      }
+    }
   }
-}
+};
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500&family=Raleway:wght@300;400;500&display=swap');
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,34 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router/router'
+import store from './store'
+import { auth } from './firebase/firebase'
+import vuetify from './plugins/vuetify';
+import vueCookies from 'vue-cookies'
+import VueClipboard from 'vue-clipboard2'
 
 Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+VueClipboard.config.autoSetContainer = true
+Vue.use(VueClipboard)
+
+let app
+auth.onAuthStateChanged(() => {
+  if (!app) {
+    app = new Vue({
+      created() {
+        document.addEventListener('beforeunload', this.handler)
+      },
+      methods: {
+        handler: async function () {
+          await auth.signOut()
+        }
+      },
+      vuetify,
+      router,
+      vueCookies,
+      store,
+      render: h => h(App)
+    }).$mount('#app')
+  }
+})
